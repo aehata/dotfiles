a04e068e1161699f8dbccea97c904551f254a923
aehata <aehata.dev@gmail.com>
2017-10-12 16:39:46 +0900

    first commit.

---
+++ .bash_profile
@@ -0,0 +1,3 @@
+if [ -x /usr/local/bin/zsh ]; then
+  exec /usr/local/bin/zsh
+fi
---
+++ .zsh/aliases.zsh
@@ -0,0 +1,37 @@
+
+### for exit
+alias q='exit'
+
+### for ls
+alias ls='ls -GwF'
+#alias ll='ls -lah'
+alias la='ls -a'
+alias ll='ls -al'
+#alias lsa='ls -al'
+
+### mistype recovery
+alias ,,='cd ../'
+alias ..='cd ..'
+alias ...='cd ../'
+
+### for zsh config
+alias viz='vi ~/.zshrc'
+alias viza='vi ~/.dotfiles/.zsh/aliases.zsh'
+
+### for docker
+alias dockps="docker ps -a"
+
+### for terraform
+alias tfplan="terraform plan"
+alias tfapply="terraform apply"
+
+### for bundler
+alias be='bundle exec'
+
+### for pipe
+alias -g G='| grep '  # e.x. dmesg lG CPU
+alias -g L='| $PAGER '
+alias -g W='| wc'
+alias -g H='| head'
+alias -g T='| tail'
+alias -g A='| awk'
