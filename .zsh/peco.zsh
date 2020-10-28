e99b4247ab10c06cac94df4de1d7287d4871a681
aehata <aehata.dev@gmail.com>
2020-10-28 20:33:07 +0900

    add conf for ghq&peco

---
+++ .zsh/peco.zsh
@@ -0,0 +1,11 @@
+function peco-cd () {
+  local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-cd
+bindkey '^]' peco-cd
+alias sd='peco-cd'
